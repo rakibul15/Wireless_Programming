7bc521086183643d2e128b168a5883dc7a91328c
Your Name <you@example.com>
2019-03-17 22:41:59 +0600

    create homepage

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('#topbar').scrollupbar();
+
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("navbar").style.height = "0%";
+  }
